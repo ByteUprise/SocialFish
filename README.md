63569fe009c5ca0a2a3bf9c84cbb68740938c86c
HackTeachz <jaypurkarpratik@gmail.com>
2020-09-16 08:50:32 +0530

    ShellFish

---
+++ README.md
@@ -0,0 +1,32 @@
+<p align="center">
+</a></p>
+<h1 align="center">SocialFish</h1>
+
+#### MAINTAINERS
+- **Alisson Moretto**, Twitter: [@HackTeachz][tw-alisson], GitHub: [@HackTeachz][git-alisson]
+- **Vandré Augusto**, Twitter: [@HackTeachz][tw-drink], GitHub: [@HackTeachz][git-drink]
+
+#### DOCS
+- **Fernando Bellincanta**, Twitter: [@HackTeachz][tw-fernando], GitHub: [@HackTeachz][git-fernando]
+
+### DISCLAIMER
+
+TO BE USED FOR EDUCATIONAL PURPOSES ONLY
+
+The use of the SocialFish is COMPLETE RESPONSIBILITY of the END-USER. Developers assume NO liability and are NOT responsible for any misuse or damage caused by this program.
+
+"DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+Taken from [LICENSE](LICENSE).
+
+
+[//]: # (links references)
+
+[tw-alisson]: <https://twitter.com/HackTeachz>
+[git-alisson]: <https://github.com/HackTeachz>
+
---
+++ core/view.py
@@ -0,0 +1,24 @@
+import colorama
+
+def head():
+    print(colorama.Style.BRIGHT)
+    print(colorama.Fore.CYAN + '''
+                          '
+                        '   '  HackTeachz | t.me/HackTeachz
+                      '       '  youtube.com/c/HackTeachz -  INDIA
+                 .  '  .        '                        '
+             '             '      '                   '   '
+  ███████ ████████ ███████ ██ ███████ ██       ███████ ██ ███████ ██   ██ 
+  ██      ██    ██ ██      ██ ██   ██ ██       ██      ██ ██      ██   ██ 
+  ███████ ██    ██ ██      ██ ███████ ██       █████   ██ ███████ ███████ 
+       ██ ██    ██ ██      ██ ██   ██ ██       ██      ██      ██ ██   ██ 
+  ███████ ████████ ███████ ██ ██   ██ ███████  ██      ██ ███████ ██   ██ 
+      .    '   '....'               ..'.      ' .
+         '  .     ,                .     '          '     '  
+ subscribe   ,       '  .  .  .  .  . '.    .'              ' 
+ hackteachz      ,          ,   ,    ,     facebook : https://facebook/HackTeachz
+                   '         '    '. '      Twitter: https://twitter.com/HackTeachz
+                     '       '      '       Site: https://HackTeach.blogspot.com
+                       ' .  '               instagram :https://instagram/HackTeachz
+                           ''')      
+    print(colorama.Fore.GREEN + 'Go to http://0.0.0.0:5000/neptune to start')
